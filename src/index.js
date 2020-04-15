4f642d78a130c4c89ebd20af929b1eac5d4149e1
David <david.izosimov@gmail.com>
2020-04-15 20:21:46 +0200

    Adds redux store and i18n to index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
+
+import { Provider } from 'react-redux';
+import configureStore from 'store/configureStore';
+
+// Styles that get put in head
+import './styles/global.scss'
+
 import App from './app';
 import * as serviceWorker from './serviceWorker';
+import './translations/i18n';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const { store } = configureStore();
+
+const rootElement = document.getElementById('root');
+const renderApp = () =>
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    rootElement
+  );
+
+// Allow hot reloading
+if (process.env.NODE_ENV !== 'production' && module.hot) {
+  module.hot.accept('./app', renderApp);
+}
+
+renderApp();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
